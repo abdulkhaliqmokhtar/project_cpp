0e35928f420c0d15b6430d5fb2a50d19d0f29ebf
abdulkhaliqmokhtar <abdulkhaliqmokhtar@gmail.com>
2022-12-24 04:44:23 +0800

    Create random integer generator function and test stored user input and random integer value

---
+++ number_guessing_game.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdlib>  // for psuedorandom sequence generation: rand() and srand()
+
+// randomNumberGenerator() will create random number on every program run
+int randomNumberGenerator()
+{
+    /*
+        srand() gives random function a new seed, a starting point;
+        usually random numbers are calculated by taking the previous number (or the seed),
+        and then do many operations on that number to generate the next
+    */
+
+   /*
+        time (0) gives time in seconds since the Unix epoch (Jan 1, 1970 - 00:00:00),
+        which is a pretty good "unpredictable" seed;
+        you're guaranteed your seed will be the same only once,
+        unless program is started multiple times within the same second
+   */
+
+    /*
+        the idea behind this snip is the default seed value for the srand() function is 1,
+        which introduce a problem: every time we run the program with seed value, output will remain the same,
+        solution: use a fresh seed value each time we run the program
+
+        What changes every second? Time. Therefore, we used the concept of the current timestamp being the current seed value
+    */
+    srand(time(0));
+
+    // lb {} is lower bound and ub {} is upper bound
+    int lb {0}, ub {100};
+
+    // generate random number on every program run within range lb to ub
+    int randomNumber = (rand() % (ub - lb + 1)) + lb;
+
+    return randomNumber;
+}
+
+int main ()
+{
+    // prompt user with integer range
+    std::cout << "Please enter an integer between 0 to 100: ";
+
+    // initialize user input as integer and take their input
+    int userNumber{};
+    std::cin >> userNumber;
+
+    // initialize random integer generated
+    int correctNumber{randomNumberGenerator()};
+
+    // output: testing purpose for user input and random number integer stored in program before proceeding
+    std::cout << "User input is: " << userNumber << '\n';
+    std::cout << "Random number is: " << correctNumber << '\n';
+
+    return 0;
+}
